70e1eaf5b95faa3102a3d17a84b65f5e82c4ffba
Talyssonbs <talyssonbs@gmail.com>
2022-09-07 20:03:52 -0300

    Adicionado o componente loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import S from "./Loading.module.css"
+const Loading = () => {
+  return (
+    <div className={S.espaco}>
+        <div className={S.loading}></div>
+    </div>
+  )
+}
+
+export default Loading
